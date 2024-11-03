b1b90a61a12f271e5e219c98a4bc6e808b1074a3
sikaiwei <kaiwei_si@hmglog.com>
2024-11-03 15:05:39 +0800

    改速度

--- app/home/daziji.tsx
+++ app/home/daziji.tsx
@@ -11,8 +11,8 @@ const TypingEffect = ({ text }) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{
-            delay: index * 0.1, // 根据字符索引设置延迟，实现逐个出现的效果
-            duration: 0.1,
+            delay: index * 0.02, // 根据字符索引设置延迟，实现逐个出现的效果
+            duration: 0.02,
             ease: "easeOut",
           }}
         >
